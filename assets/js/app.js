ec2a8e39fb4eb3b9fa59a12b83cb0056d99f4228
DT <dmofot@gmail.com>
2015-09-03 16:59:41 -0400

    update to v2.o

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,16 +1,72 @@
-var map, featureList, poiSearch = [];
+var map, featureList, trailSearch = [], brewerySearch = [], otherSearch = [];
 
-$(document).on("click", ".feature-name", function(e) {
-  sidebarClick(parseInt($(this).attr('id')));
+$(window).resize(function() {
+  sizeLayerControl();
 });
 
+$(document).on("click", ".feature-row", function(e) {
+  $(document).off("mouseout", ".feature-row", clearHighlight);
+  sidebarClick(parseInt($(this).attr("id"), 10));
+});
+
+$(document).on("mouseover", ".feature-row", function(e) {
+  highlight.clearLayers().addLayer(L.circleMarker([$(this).attr("lat"), $(this).attr("lng")], highlightStyle));
+});
+
+$(document).on("mouseout", ".feature-row", clearHighlight);
+
+$("#about-btn").click(function() {
+  $("#aboutModal").modal("show");
+  $(".navbar-collapse.in").collapse("hide");
+  return false;
+});
+
+$("#full-extent-btn").click(function() {
+  map.fitBounds(trail.getBounds());
+  $(".navbar-collapse.in").collapse("hide");
+  return false;
+});
+
+$("#legend-btn").click(function() {
+  $("#legendModal").modal("show");
+  $(".navbar-collapse.in").collapse("hide");
+  return false;
+});
+
+$("#list-btn").click(function() {
+  $('#sidebar').toggle();
+  map.invalidateSize();
+  return false;
+});
+
+$("#nav-btn").click(function() {
+  $(".navbar-collapse").collapse("toggle");
+  return false;
+});
+
+$("#sidebar-toggle-btn").click(function() {
+  $("#sidebar").toggle();
+  map.invalidateSize();
+  return false;
+});
+
+$("#sidebar-hide-btn").click(function() {
+  $('#sidebar').hide();
+  map.invalidateSize();
+});
+
+function sizeLayerControl() {
+  $(".leaflet-control-layers").css("max-height", $("#map").height() - 50);
+}
+
+function clearHighlight() {
+  highlight.clearLayers();
+}
+
 function sidebarClick(id) {
-  map.addLayer(poiLayer);
   var layer = markerClusters.getLayer(id);
-  markerClusters.zoomToShowLayer(layer, function() {
-    map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 17);
-    layer.fire("click");
-  });
+  map.setView([layer.getLatLng().lat, layer.getLatLng().lng], 17);
+  layer.fire("click");
   /* Hide sidebar and go to the map on small screens */
   if (document.body.clientWidth <= 767) {
     $("#sidebar").hide();
@@ -18,70 +74,75 @@ function sidebarClick(id) {
   }
 }
 
-/* Basemap Layers
-   Example basemaps from Mapbox */
-var mapboxTer = L.tileLayer("//{s}.tiles.mapbox.com/v3/matt.72ef5189/{z}/{x}/{y}.png", {
-  detectRetina: 'true',
-  maxZoom: 21,
+function syncSidebar() {
+  /* Empty sidebar features */
+  $("#feature-list tbody").empty();
+  /* Loop through brewery layer and add only features which are in the map bounds */
+  brewery.eachLayer(function (layer) {
+    if (map.hasLayer(breweryLayer)) {
+      if (map.getBounds().contains(layer.getLatLng())) {
+        $("#feature-list tbody").append('<tr class="feature-row" id="' + L.stamp(layer) + '" lat="' + layer.getLatLng().lat + '" lng="' + layer.getLatLng().lng + '"><td style="vertical-align: middle;"><img width="16" height="18" src="assets/img/brewery.png"></td><td class="feature-name">' + layer.feature.properties.Name + '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
+      }
+    }
+  });
+  /* Loop through other layer and add only features which are in the map bounds */
+  other.eachLayer(function (layer) {
+    if (map.hasLayer(otherLayer)) {
+      if (map.getBounds().contains(layer.getLatLng())) {
+        $("#feature-list tbody").append('<tr class="feature-row" id="' + L.stamp(layer) + '" lat="' + layer.getLatLng().lat + '" lng="' + layer.getLatLng().lng + '"><td style="vertical-align: middle;"><img width="16" height="18" src="assets/img/other.png"></td><td class="feature-name">' + layer.feature.properties.Name + '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
+      }
+    }
+  });
+  /* Update list.js featureList */
+  featureList = new List("features", {
+    valueNames: ["feature-name"]
+  });
+  featureList.sort("feature-name", {
+    order: "asc"
+  });
+}
+
+/* Basemap Layers */
+var mapboxTer = L.tileLayer("http://{s}.tiles.mapbox.com/v3/matt.72ef5189/{z}/{x}/{y}.png", {
+  maxZoom: 19,
   subdomains: ["a", "b", "c", "d"],
   attribution: 'Tiles courtesy of <a href="http://www.mapbox.com/" target="_blank">Mapbox</a> '
 });
-
-var mapboxSat = L.tileLayer("//{s}.tiles.mapbox.com/v3/examples.map-qfyrx5r8/{z}/{x}/{y}.png", {
-  detectRetina: 'true',
-  maxZoom: 21,
+var mapboxSat = L.tileLayer("http://{s}.tiles.mapbox.com/v3/examples.map-qfyrx5r8/{z}/{x}/{y}.png", {
+  maxZoom: 18,
   subdomains: ["a", "b", "c", "d"],
   attribution: 'Tiles courtesy of <a href="http://www.mapbox.com/" target="_blank">Mapbox</a> '
 });
 
 /* Overlay Layers */
 var highlight = L.geoJson(null);
+var highlightStyle = {
+  stroke: false,
+  fillColor: "#00FFFF",
+  fillOpacity: 0.7,
+  radius: 10
+};
 
-var mtvernonTrail = L.geoJson(null, {
+var trail = L.geoJson(null, {
   style: function (feature) {
-  	return {
-  	  color: "#ff3135",
+    return {
+      color: "#ff3135",
       opacity: 0.6,
-      weight: 3
+      weight: 3,
+      clickable: false
     };
   },
   onEachFeature: function (feature, layer) {
-    if (feature.properties) {
-      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Name</th><td>" + feature.properties.name + "<table>";
-      layer.on({
-        click: function (e) {
-          $("#feature-title").html(feature.properties.name);
-          $("#feature-info").html(content);
-          $("#featureModal").modal("show");
-          highlight.clearLayers().addLayer(L.circleMarker([e.latlng.lat, e.latlng.lng], {
-            stroke: false,
-            fillColor: "#00FFFF",
-            fillOpacity: 0.7,
-            radius: 10
-          }));
-        }
-      });
-    }
-    layer.on({
-      mouseover: function (e) {
-        var layer = e.target;
-        layer.setStyle({
-          weight: 3,
-          color: "#00FFFF",
-          opacity: 1
-        });
-        if (!L.Browser.ie && !L.Browser.opera) {
-          layer.bringToFront();
-        }
-      },
-      mouseout: function (e) {
-        mtvernonTrail.resetStyle(e.target);
-      }
+    trailSearch.push({
+      name: layer.feature.properties.name,
+      source: "Trail",
+      id: L.stamp(layer),
+      bounds: layer.getBounds()
     });
   }
 });
-$.getJSON("data/mtvernon.geojson", function (data) {
-  mtvernonTrail.addData(data);
+$.getJSON("data/trail.geojson", function (data) {
+  trail.addData(data);
 });
 
 /* Single marker cluster layer to hold all clusters */
@@ -92,13 +153,13 @@ var markerClusters = new L.MarkerClusterGroup({
   disableClusteringAtZoom: 16
 });
 
-/* Empty layer placeholder to add to layer control for listening when to add/remove pois to markerClusters layer */
-var poiLayer = L.geoJson(null);
-var pois = L.geoJson(null, {
+/* Empty layer placeholder to add to layer control for listening when to add/remove brewery to markerClusters layer */
+var breweryLayer = L.geoJson(null);
+var brewery = L.geoJson(null, {
   pointToLayer: function (feature, latlng) {
     return L.marker(latlng, {
       icon: L.icon({
-        iconUrl: "assets/img/beer.png",
+        iconUrl: "assets/img/brewery.png",
         iconSize: [24, 28],
         iconAnchor: [12, 28],
         popupAnchor: [0, -25]
@@ -115,19 +176,14 @@ var pois = L.geoJson(null, {
           $("#feature-title").html(feature.properties.Name);
           $("#feature-info").html(content);
           $("#featureModal").modal("show");
-          highlight.clearLayers().addLayer(L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
-            stroke: false,
-            fillColor: "#00FFFF",
-            fillOpacity: 0.7,
-            radius: 10
-          }));
+          highlight.clearLayers().addLayer(L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], highlightStyle));
         }
       });
-      $("#feature-list tbody").append('<tr style="cursor: pointer;"><td style="vertical-align: middle;"><img width="16" height="18" src="assets/img/beer.png"></td><td class="feature-name" id="'+L.stamp(layer)+'">'+layer.feature.properties.Name+'</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
-      poiSearch.push({
+      $("#feature-list tbody").append('<tr class="feature-row" id="' + L.stamp(layer) + '" lat="' + layer.getLatLng().lat + '" lng="' + layer.getLatLng().lng + '"><td style="vertical-align: middle;"><img width="16" height="18" src="assets/img/brewery.png"></td><td class="feature-name">' + layer.feature.properties.Name + '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
+      brewerySearch.push({
         name: layer.feature.properties.Name,
         address: layer.feature.properties.Address,
-        source: "Places",
+        source: "Brewery",
         id: L.stamp(layer),
         lat: layer.feature.geometry.coordinates[1],
         lng: layer.feature.geometry.coordinates[0]
@@ -135,30 +191,88 @@ var pois = L.geoJson(null, {
     }
   }
 });
-$.getJSON("data/pois.geojson", function (data) {
-  pois.addData(data);
-  map.addLayer(poiLayer);
+$.getJSON("data/brewery.geojson", function (data) {
+  brewery.addData(data);
+  map.addLayer(breweryLayer);
+});
+
+/* Empty layer placeholder to add to layer control for listening when to add/remove other to markerClusters layer */
+var otherLayer = L.geoJson(null);
+var other = L.geoJson(null, {
+  pointToLayer: function (feature, latlng) {
+    return L.marker(latlng, {
+      icon: L.icon({
+        iconUrl: "assets/img/other.png",
+        iconSize: [24, 28],
+        iconAnchor: [12, 28],
+        popupAnchor: [0, -25]
+      }),
+      title: feature.properties.Name,
+      riseOnHover: true
+    });
+  },
+  onEachFeature: function (feature, layer) {
+    if (feature.properties) {
+      var content = "<table class='table table-striped table-bordered table-condensed'>" + "<tr><th>Name</th><td>" + feature.properties.Name + "</td></tr>" + "<tr><th>Address</th><td>" + feature.properties.Address + "</td></tr>" + "<tr><th>Mile Marker Exit</th><td>" + feature.properties.MileMarkerExit + "</td></tr>" + "<tr><th>Tap Count</th><td>" + feature.properties.Taps + "</td></tr>" + "<tr><th>Website</th><td><a class='url-break' href='" + feature.properties.URL + "' target='_blank'>" + feature.properties.URL + "</a></td></tr>" + "<tr><th>Hours</th><td>" + feature.properties.Open + "</td></tr>" + "<tr><th>Directions</th><td>" + "Located " + feature.properties.MilesFromTrail + " miles from trail. " + feature.properties.Directions + "</td></tr>" + "<table>";
+      layer.on({
+        click: function (e) {
+          $("#feature-title").html(feature.properties.Name);
+          $("#feature-info").html(content);
+          $("#featureModal").modal("show");
+          highlight.clearLayers().addLayer(L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], highlightStyle));
+        }
+      });
+      $("#feature-list tbody").append('<tr class="feature-row" id="' + L.stamp(layer) + '" lat="' + layer.getLatLng().lat + '" lng="' + layer.getLatLng().lng + '"><td style="vertical-align: middle;"><img width="16" height="18" src="assets/img/other.png"></td><td class="feature-name">' + layer.feature.properties.Name + '</td><td style="vertical-align: middle;"><i class="fa fa-chevron-right pull-right"></i></td></tr>');
+      otherSearch.push({
+        name: layer.feature.properties.Name,
+        address: layer.feature.properties.Address,
+        source: "Other",
+        id: L.stamp(layer),
+        lat: layer.feature.geometry.coordinates[1],
+        lng: layer.feature.geometry.coordinates[0]
+      });
+    }
+  }
+});
+$.getJSON("data/other.geojson", function (data) {
+  other.addData(data);
+  map.addLayer(otherLayer);
 });
 
 map = L.map("map", {
-  zoom: 11,
-  center: [38.793298, -77.037722],
-  layers: [mapboxTer, mtvernonTrail, markerClusters, highlight],
+  zoom: 10,
+  center: [40.702222, -73.979378],
+  layers: [mapboxTer, trail, markerClusters, highlight],
   zoomControl: false,
   attributionControl: false
 });
 
 /* Layer control listeners that allow for a single markerClusters layer */
 map.on("overlayadd", function(e) {
-  if (e.layer === poiLayer) {
-    markerClusters.addLayer(pois);
+  if (e.layer === breweryLayer) {
+    markerClusters.addLayer(brewery);
+    syncSidebar();
+  }
+  if (e.layer === otherLayer) {
+    markerClusters.addLayer(other);
+    syncSidebar();
   }
 });
 
 map.on("overlayremove", function(e) {
-  if (e.layer === poiLayer) {
-    markerClusters.removeLayer(pois);
+  if (e.layer === breweryLayer) {
+    markerClusters.removeLayer(brewery);
+    syncSidebar();
   }
+  if (e.layer === otherLayer) {
+    markerClusters.removeLayer(other);
+    syncSidebar();
+  }
+});
+
+/* Filter sidebar feature list to only show features in current map bounds */
+map.on("moveend", function (e) {
+  syncSidebar();
 });
 
 /* Clear feature highlight when map is clicked */
@@ -182,7 +296,7 @@ var attributionControl = L.control({
 });
 attributionControl.onAdd = function (map) {
   var div = L.DomUtil.create("div", "leaflet-control-attribution");
-  div.innerHTML = "<span class='hidden-xs'>Developed by <a href='https://davidtodd.info'>DT</a> | </span><a href='#' onclick='$(\"#attributionModal\").modal(\"show\"); return false;'>Attribution</a>";
+  div.innerHTML = "<span class='hidden-xs'>Developed by <a href='http://davidtodd.info'>DT</a> | </span><a href='#' onclick='$(\"#attributionModal\").modal(\"show\"); return false;'>Attribution</a>";
   return div;
 };
 map.addControl(attributionControl);
@@ -207,7 +321,7 @@ var locateControl = L.control.locate({
     weight: 1,
     clickable: false
   },
-  icon: "icon-direction",
+  icon: "fa fa-location-arrow",
   metric: false,
   strings: {
     title: "My location",
@@ -231,16 +345,17 @@ if (document.body.clientWidth <= 767) {
 }
 
 var baseLayers = {
-  "Terrain": mapboxTer,
+  "Street Map": mapboxTer,
   "Imagery": mapboxSat
 };
 
 var groupedOverlays = {
   "Points of Interest": {
-    "<img src='assets/img/beer.png' width='24' height='28'>&nbsp;Places": poiLayer
+    "<img src='assets/img/brewery.png' width='24' height='28'>&nbsp;Brewery": breweryLayer,
+    "<img src='assets/img/other.png' width='24' height='28'>&nbsp;Other": otherLayer
   },
   "Reference": {
-    "Mt. Vernon Trail": mtvernonTrail
+    "Trail": trail
   }
 };
 
@@ -253,21 +368,53 @@ $("#searchbox").click(function () {
   $(this).select();
 });
 
+/* Prevent hitting enter from refreshing the page */
+$("#searchbox").keypress(function (e) {
+  if (e.which == 13) {
+    e.preventDefault();
+  }
+});
+
+$("#featureModal").on("hidden.bs.modal", function (e) {
+  $(document).on("mouseout", ".feature-row", clearHighlight);
+});
+
 /* Typeahead search functionality */
 $(document).one("ajaxStop", function () {
   $("#loading").hide();
+  sizeLayerControl();
   /* Fit map to trail bounds */
-  map.fitBounds(mtvernonTrail.getBounds());
+  map.fitBounds(trail.getBounds());
   featureList = new List("features", {valueNames: ["feature-name"]});
   featureList.sort("feature-name", {order:"asc"});
 
-  var poiBH = new Bloodhound({
-    name: "Places",
+  var trailBH = new Bloodhound({
+    name: "Trail",
     datumTokenizer: function (d) {
       return Bloodhound.tokenizers.whitespace(d.name);
     },
     queryTokenizer: Bloodhound.tokenizers.whitespace,
-    local: poiSearch,
+    local: trailSearch,
+    limit: 10
+  });
+
+  var breweryBH = new Bloodhound({
+    name: "Brewery",
+    datumTokenizer: function (d) {
+      return Bloodhound.tokenizers.whitespace(d.name);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: brewerySearch,
+    limit: 10
+  });
+
+  var otherBH = new Bloodhound({
+    name: "Other",
+    datumTokenizer: function (d) {
+      return Bloodhound.tokenizers.whitespace(d.name);
+    },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: otherSearch,
     limit: 10
   });
 
@@ -301,7 +448,9 @@ $(document).one("ajaxStop", function () {
     },
     limit: 10
   });
-  poiBH.initialize();
+  trailBH.initialize();
+  breweryBH.initialize();
+  otherBH.initialize();
   geonamesBH.initialize();
 
   /* instantiate the typeahead UI */
@@ -310,11 +459,26 @@ $(document).one("ajaxStop", function () {
     highlight: true,
     hint: false
   }, {
-    name: "Places",
+    name: "Trail",
     displayKey: "name",
-    source: poiBH.ttAdapter(),
+    source: trailBH.ttAdapter(),
     templates: {
-      header: "<h4 class='typeahead-header'><img src='assets/img/beer.png' width='24' height='28'>&nbsp;Places</h4>",
+      header: "<h4 class='typeahead-header'>Trail</h4>"
+    }
+  }, {
+    name: "Brewery",
+    displayKey: "name",
+    source: breweryBH.ttAdapter(),
+    templates: {
+      header: "<h4 class='typeahead-header'><img src='assets/img/brewery.png' width='24' height='28'>&nbsp;Brewery</h4>",
+      suggestion: Handlebars.compile(["{{name}}<br>&nbsp;<small>{{address}}</small>"].join(""))
+    }
+  }, {
+    name: "Other",
+    displayKey: "name",
+    source: otherBH.ttAdapter(),
+    templates: {
+      header: "<h4 class='typeahead-header'><img src='assets/img/other.png' width='24' height='28'>&nbsp;Other</h4>",
       suggestion: Handlebars.compile(["{{name}}<br>&nbsp;<small>{{address}}</small>"].join(""))
     }
   }, {
@@ -325,9 +489,21 @@ $(document).one("ajaxStop", function () {
       header: "<h4 class='typeahead-header'><img src='assets/img/globe.png' width='25' height='25'>&nbsp;GeoNames</h4>"
     }
   }).on("typeahead:selected", function (obj, datum) {
-    if (datum.source === "Places") {
-      if (!map.hasLayer(poiLayer)) {
-        map.addLayer(poiLayer);
+    if (datum.source === "Trail") {
+      map.fitBounds(datum.bounds);
+    }
+    if (datum.source === "Brewery") {
+      if (!map.hasLayer(breweryLayer)) {
+        map.addLayer(breweryLayer);
+      }
+      map.setView([datum.lat, datum.lng], 17);
+      if (map._layers[datum.id]) {
+        map._layers[datum.id].fire("click");
+      }
+    }
+    if (datum.source === "Other") {
+      if (!map.hasLayer(otherLayer)) {
+        map.addLayer(otherLayer);
       }
       map.setView([datum.lat, datum.lng], 17);
       if (map._layers[datum.id]) {
@@ -350,3 +526,13 @@ $(document).one("ajaxStop", function () {
   $(".twitter-typeahead").css("position", "static");
   $(".twitter-typeahead").css("display", "block");
 });
+
+// Leaflet patch to make layer control scrollable on touch browsers
+var container = $(".leaflet-control-layers")[0];
+if (!L.Browser.touch) {
+  L.DomEvent
+  .disableClickPropagation(container)
+  .disableScrollPropagation(container);
+} else {
+  L.DomEvent.disableClickPropagation(container);
+}
